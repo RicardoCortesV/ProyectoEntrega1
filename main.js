7665c85570da3832201086023ac9aa72dd0354fb
RicardoCortesV <iudokinfo@gmail.com>
2022-07-17 21:52:11 -0500

    Nuevas funciones simulando pedidos y validando stock

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-function mayorEdad() {
-    alert('Para comprar, debes ser mayor de edad');
-    let edad = prompt("edad?")
-    if (edad >= 18) {
-        alert('Adelante, puedes comprar');
+function agregarAlCarrito(producto, stockDelProducto) {
+    const tenemosStock = validarStock(stockDelProducto);
+    if (tenemosStock === 'SÌ hay stock') {
+        console.log('Agregaste ' + stockDelProducto + ' unidades de ' + producto);
     } else {
-        alert('NO puedes comprar');
+        console.log('lo siento, no tenemos stock suficiente de ' + producto);
     }
-    if (edad >= 18) {
-        alert('Escribe lo que requieres');
-        let stock = prompt('cantidad')
-        if (stock >= 10) {
-            alert('Lo sentimos, no hay suficiente stock')
-        } else {
-            let compras = ["alegrías, galletas, Pan"];
-            for (let i = 0; i < compras.length; i++) {
-                alert("Esta es la lista de los artìculos: " + compras[i]);
-            }
 
-        }
+}
 
-
+function validarStock(stockDelProducto) {
+    if (stockDelProducto > 10) {
+        return 'NO hay stock';
+    } else {
+        return 'SÌ hay stock';
     }
 }
+
+stock = ["Alegrías, Pan, Dulces"]
+
+for (let i = 0; i < stock.length; i++) {
+    alert("Éstos artículos tienen poco stock: " + stock[i]);
+}
+
+
+
+
+
 
 
 
